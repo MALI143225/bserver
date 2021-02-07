b3c19140bf3bf8b08974c82b2b3c2eafe37b97b2
Keshav <keshavchenna231@gmail.com>
2021-02-07 20:25:50 +0530

    init

---
+++ util/validators.js
@@ -0,0 +1,41 @@
+module.exports.validateRegisterInput = (
+    username,
+    email,
+    password,
+    confirmPassword
+)=>{
+    const errors = {};
+    if (username.trim() === ''){
+        errors.username = "Username must not be empty";
+    }
+    if (email.trim() === ''){
+        errors.email = "Email must not be empty";
+    }else{
+        const regEx = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
+        if (!email.match(regEx)){
+            errors.email = "Email must be a valid email address";
+        }
+    }
+    if (password === ''){
+        errors.password = "Password must not be empty";
+    }else if (password !== confirmPassword){
+            errors.confirmPassword = "Passwords must Match";
+        }
+    return {
+        errors, 
+        valid:Object.keys(errors).length < 1
+    };
+};
+module.exports.validateLoginInput = (username,password) => {
+    const errors = {};
+    if (username.trim() === ''){
+        errors.username = "Username must not be empty";
+    }
+    if (password.trim() === ''){
+        errors.password = "Password must not be empty";
+    }
+    return {
+        errors, 
+        valid:Object.keys(errors).length < 1
+    };
+}
